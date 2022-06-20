31dbc48a50cd1e37c5733339f1c70a482ead5312
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-06-20 23:44:34 +0300

    Added my index.html files and other assets

---
+++ assets/index.js
@@ -0,0 +1,4 @@
+const h1 = document.createElement("h1");
+h1.textContent = "Best Vector Artist in Instagram"
+const headingDiv = document.getElementsByClassName('vectorHeading');
+headingDiv.appendChild(h1)
